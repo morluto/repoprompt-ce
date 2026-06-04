c7f6a57a1c1fc0c127722fd32b539ee0d6988767
Eric Provencher <eprovencher92@gmail.com>
2026-06-04 17:08:23 -0400

    Prepare RepoPrompt CE 1.0.6 release

--- Sources/RepoPromptMCP/main.swift
+++ Sources/RepoPromptMCP/main.swift
@@ -9,7 +9,7 @@ import SystemPackage
 // MARK: - Version Constants
 
 /// Update this when releasing new versions
-let CLI_VERSION = "1.0.5"
+let CLI_VERSION = "1.0.6"
 
 /// CLI verbose mode - controls debug output (enabled by --verbose flag)
 var cliVerboseMode = false
